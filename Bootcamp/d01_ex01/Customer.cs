cc8f6dc35588a28076e2053d7854ba8efed5d9dc
decordel <mrroudenr@gmail.com>
2023-12-01 17:44:31 +0300

    feat: day 01 ex01

---
+++ Bootcamp/d01_ex01/Customer.cs
@@ -0,0 +1,36 @@
+﻿namespace d01_ex01;
+
+public class Customer(string name, int id)
+{
+	private int Id { get; } = id;
+	private string Name { get; } = name;
+
+	public override string ToString()
+	{
+		return new string($"{Name}, customer #{Id}");
+	}
+
+	public override bool Equals(object? obj)
+	{
+		return obj is Customer customer && Equals(customer);
+	}
+
+	private bool Equals(Customer other)
+	{
+		return Id == other.Id && Name == other.Name;
+	}
+	
+	public static bool operator ==(Customer? left, Customer? right)
+	{
+		return Equals(left, right);
+	}
+	public static bool operator !=(Customer? left, Customer? right)
+	{
+		return !Equals(left, right);
+	}
+	
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Id, Name);
+	}
+}
---
+++ Bootcamp/d01_ex01/Program.cs
@@ -0,0 +1,36 @@
+﻿using d01_ex01;
+
+try
+{
+	Test(TestToStringCustomer);
+	Test(TestSameCustomer);
+}
+catch (Exception e)
+{
+	Console.WriteLine(e);
+}
+
+bool TestToStringCustomer()
+{
+	var customer = new Customer("Andrew", 1);
+	return customer.ToString() == "Andrew, customer #1";
+}
+
+bool TestSameCustomer()
+{
+	var customer1 = new Customer("Andrew", 1);
+	var customer2 = new Customer("Andrew", 1);
+	return customer1 == customer2;
+}
+
+void Test(Func<bool> testFunc)
+{
+	if (testFunc())
+	{
+		Console.WriteLine($"{testFunc.Method.Name} passed");
+	}
+	else
+	{
+		throw new Exception($"{testFunc.Method.Name} failed");
+	}
+}
