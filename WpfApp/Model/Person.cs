6b15e7ade1cec9d3bdacd473c604d25769da98c1
decordel <mrroudenr@gmail.com>
2023-12-01 17:01:26 +0300

    Add WPF Application to the solution
    Added a new WPF application to the solution with structure for MVVM pattern, necessary dependencies and settings. This WPF application will serve as the UI for our project. It includes a model for Person, a main window and a view model that supports adding person objects to an observable collection.

---
+++ WpfApp/Model/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfApp.Model;
+
+public class Person
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+}
---
+++ WpfApp/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace WpfApp.ViewModel;
+
+public class ViewModelLocator
+{
+	static ViewModelLocator()
+	{
+		ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+		SimpleIoc.Default.Register<MainViewModel>();
+	}
+	
+	public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+}
