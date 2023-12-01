7540798d2645e35a628924d07edfbef8449e5ba9
decordel <mrroudenr@gmail.com>
2023-12-01 17:24:34 +0300

    feat: day 01 ex00

---
+++ Bootcamp/d01_ex00/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using d01_ex00;
+
+Test(TestEmptyStorage);
+return;
+
+// Тестовые Функции
+bool TestEmptyStorage()
+{
+	Storage storage = new Storage(10);
+	return storage.IsEmpty;
+}
+
+void Test(Func<bool> testFunc)
+{
+	if (testFunc())
+	{
+		Console.WriteLine($"{testFunc.Method.Name} passed");
+	}
+	else
+	{
+		throw new Exception($"{testFunc.Method.Name} failed");
+	}
+}
---
+++ Bootcamp/d01_ex00/Storage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace d01_ex00;
+
+public class Storage
+{
+	private int Goods { get; set; }
+	private int Capacity { get; set; }
+	
+	public Storage(int capacity)
+	{
+		if (capacity <= 0)
+		{
+			throw new ArgumentException("Capacity must be greater than 0");
+		}
+		
+		Capacity = capacity;
+		Goods = 0;
+	}
+	
+	public bool IsEmpty => Goods == 0;
+}
