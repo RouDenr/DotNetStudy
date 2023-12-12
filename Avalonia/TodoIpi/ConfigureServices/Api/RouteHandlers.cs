ad00acd7703e47a8a74412f88a6f67da5548ae3b
decordel <programmer.pg22@gmail.com>
2023-12-12 15:31:14 +0300

    fix: refactor Api Command logic to Attribute statement

---
+++ Avalonia/TodoIpi/ConfigureServices/Api/RouteHandlers.cs
@@ -0,0 +1,14 @@
+﻿using TodoIpi.Weather;
+
+namespace TodoIpi.ConfigureServices.Api;
+
+public static class RouteHandlers
+{
+	[ApiCommand("/weatherforecast", "GetWeatherForecast", "GET")]
+	public static async Task GetWeatherForecast(HttpContext context, 
+		IWeatherForecastGenerator forecastGenerator)
+	{
+		var forecasts = forecastGenerator.Generate();
+		await context.Response.WriteAsJsonAsync(forecasts);
+	}
+}
--- Avalonia/TodoIpi/Program.cs
+++ Avalonia/TodoIpi/Program.cs
@@ -1,37 +1,13 @@
 
-using TodoIpi.Weather;
+using TodoIpi.ConfigureServices;
 
 // Создаём новый экземпляр билдера приложения с передачей аргументов командной строки
 var builder = WebApplication.CreateBuilder(args);
+Startup startup = new();
+startup.ConfigureServices(builder.Services);
 
-// Добавляем в контейнер сервисы для работы с API
-builder.Services.AddEndpointsApiExplorer();
-
-// Добавляем в контейнер сервисы для работы со Swagger для описания и документирования API
-// Дополнительную информацию о настройке Swagger/OpenAPI можно найти тут: https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen();
-
-// Строим приложение из билдера
-builder.Services.AddSingleton<IWeatherForecastGenerator, WeatherForecastGenerator>();
 WebApplication app = builder.Build();
-
-// Добавляем конфигурацию промежуточного программного обеспечения (middleware)
-if (app.Environment.IsDevelopment())
-{
-    // Если приложение в режиме разработки, то используем Swagger для автогенерации документации API
-    app.UseSwagger();
-    // И специальный UI для просмотра этой документации
-    app.UseSwaggerUI();
-}
-
-// Все HTTP запросы будут автоматически переадресовываться на HTTPS
-app.UseHttpsRedirection();
-
-
-// Определяем HTTP GET запрос по пути "/weatherforecast" 
-app.MapGet("/weatherforecast", (IWeatherForecastGenerator forecastGenerator) => forecastGenerator.Generate())
-.WithName("GetWeatherForecast") // Даем имя этому маршруту
-.WithOpenApi(); // И открываем его в OpenAPI 
+startup.Configure(app, builder.Environment);
 
 // Запуск приложения
 app.Run();
