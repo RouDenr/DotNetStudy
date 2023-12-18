1f4cc295c5c5b74f4040a972770cd95c71027b66
decordel <programmer.pg22@gmail.com>
2023-12-18 17:14:27 +0300

    feat: add CashRegister tests

--- Bootcamp/d01_ex04/Program.cs
+++ Bootcamp/d01_ex04/Program.cs
@@ -7,6 +7,9 @@ try
 	Test(TestSameCustomer);
 	Test(TestDifferentCustomer);
 	Test(TestCreateRandomCard);
+	Test(TestSameCashRegister);
+	Test(TestDifferentCashRegister);
+	Test(TestCashRegisterWithMinCustomers);
 }
 catch (Exception e)
 {
@@ -51,20 +54,82 @@ bool TestCreateRandomCard()
 	Console.WriteLine(customer);
 	if (customer.GoodsCount is <= 0 or > 15)
 	{
+		Console.WriteLine("Goods count is out of range");
 		return false;
 	}
 	customer2.FillCart(15);
 	Console.WriteLine(customer2);
 	if (customer2.GoodsCount is <= 0 or > 15)
 	{
+		Console.WriteLine("Goods count is out of range");
 		return false;
 	}
 	customer3.FillCart(15);
 	Console.WriteLine(customer3);
-	return customer3.GoodsCount is > 0 and <= 15;
+	
+	if (customer3.GoodsCount is > 0 and <= 15)
+	{
+		return true;
+	}
+	Console.WriteLine("Goods count is out of range");
+	return false;
+}
+
+bool TestSameCashRegister()
+{
+	var cashRegister1 = new CashRegister("Register #1");
+	var cashRegister2 = new CashRegister("Register #1");
+	return cashRegister1 == cashRegister2;
+}
+
+bool TestDifferentCashRegister()
+{
+	var cashRegister1 = new CashRegister("Register #1");
+	var cashRegister2 = new CashRegister("Register #2");
+	return cashRegister1 != cashRegister2;
 }
 
 
+bool TestCashRegisterWithMinCustomers()
+{
+	var cashRegister1 = new CashRegister("Register #1");
+	var cashRegister2 = new CashRegister("Register #2");
+	var cashRegister3 = new CashRegister("Register #3");
+	var cashRegister4 = new CashRegister("Register #4");
+	var cashRegister5 = new CashRegister("Register #5");
+	
+	cashRegister1.AddCustomer(new Customer("Andrew", 1));
+	cashRegister1.AddCustomer(new Customer("Bob", 2));
+	cashRegister1.AddCustomer(new Customer("Charlie", 3));
+	
+	cashRegister2.AddCustomer(new Customer("Andrew", 1));
+	cashRegister2.AddCustomer(new Customer("Bob", 2));
+	
+	cashRegister3.AddCustomer(new Customer("Andrew", 1));
+	cashRegister3.AddCustomer(new Customer("Bob", 2));
+	cashRegister3.AddCustomer(new Customer("Charlie", 3));
+	cashRegister3.AddCustomer(new Customer("David", 4));
+	
+	cashRegister4.AddCustomer(new Customer("Andrew", 1));
+	cashRegister4.AddCustomer(new Customer("Bob", 2));
+	cashRegister4.AddCustomer(new Customer("Charlie", 3));
+	cashRegister4.AddCustomer(new Customer("David", 4));
+	cashRegister4.AddCustomer(new Customer("Ethan", 5));
+	
+	cashRegister5.AddCustomer(new Customer("Andrew", 1));
+	cashRegister5.AddCustomer(new Customer("Bob", 2));
+	cashRegister5.AddCustomer(new Customer("Charlie", 3));
+	cashRegister5.AddCustomer(new Customer("David", 4));
+	cashRegister5.AddCustomer(new Customer("Ethan", 5));
+	cashRegister5.AddCustomer(new Customer("Frank", 6));
+	
+	var cashRegisters = new[] {cashRegister1, cashRegister2, cashRegister3, cashRegister4, cashRegister5};
+	var minQueue = CustomerExtensions.CashRegisterWithMinCustomers(cashRegisters);
+	return minQueue == cashRegister2;
+}
+	
+	
+	
 void Test(Func<bool> testFunc)
 {
 	if (testFunc())
