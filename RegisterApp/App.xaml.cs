9e0cda14ce30655930f307a7a042088f947c4168
decordel <programmer.pg22@gmail.com>
2024-01-30 18:21:45 +0300

    feat: init logger

--- RegisterApp/App.xaml.cs
+++ RegisterApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using NLog;
 
 namespace RegisterApp;
 
@@ -9,4 +10,21 @@ namespace RegisterApp;
 /// </summary>
 public partial class App : Application
 {
+	private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+	public App()
+	{
+		var logConfig = new NLog.Config.LoggingConfiguration();
+		
+		var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "log.txt" };
+		var logConsole = new NLog.Targets.ConsoleTarget("logconsole");
+		
+		logConfig.AddRule(LogLevel.Info, LogLevel.Fatal, logConsole);
+		logConfig.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+		
+		LogManager.Configuration = logConfig;
+		
+		Logger.Info("Program started");
+	}
+	
 }
