b11ae075276eb28401b19c04a880d82e505fd76f
decordel <programmer.pg22@gmail.com>
2024-01-30 18:22:23 +0300

    refactor: move pass manage to db context

--- RegisterApp/Model/UserDbContext.cs
+++ RegisterApp/Model/UserDbContext.cs
@@ -5,23 +5,30 @@ namespace RegisterApp.Model
 {
 	public class UserDbContext : DbContext
 	{
-        private const string DbNAme = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=1234";
+		
+        private const string DbNAme = "Host=localhost;Port=5433;Database=users;Username=postgres;Password=4321";
 		public DbSet<User> Users { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(DbNAme);
-       
-            // check connection
-   //          try
-			// {
-			// 	Database.OpenConnection();
-			// 	Database.CloseConnection();
-			// }
-			// catch
-			// {
-			// 	Database.EnsureCreated();
-			// }
+        }
+        
+        
+		
+        public static byte[] GeneratePasswordHash(string password, byte[] salt)
+        {
+	        // Hash the password and encode the parameters.
+	        return new System.Security.Cryptography.Rfc2898DeriveBytes(password, salt, 10000).GetBytes(32);
+        }
+        
+        public static byte[] GenerateSalt()
+        {
+	        var buff = new byte[32];
+			
+	        using var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
+	        rng.GetBytes(buff);
+			
+	        return buff;
         }
     }
 } 
--- RegisterApp/ViewModel/RegistrationViewModel.cs
+++ RegisterApp/ViewModel/RegistrationViewModel.cs
@@ -1,20 +1,36 @@
 using System;
 using System.Windows.Input;
+using RegisterApp.Model;
 
 namespace RegisterApp.ViewModel
 {
-	public class RegistrationViewModel
+	public class RegistrationViewModel : ViewModelBase
 	{
+        
+		private string _login = string.Empty;
+		private string _password = string.Empty;
+        
+		public string Login { get => _login; set => RaiseAndSetIfChanged(ref _login, value, nameof(Login)); }
+		public string Password { get => _password; set => RaiseAndSetIfChanged(ref _password, value, nameof(Password)); }
+
 		public ICommand RegistrationCommand { get; }
 
 		public RegistrationViewModel()
 		{
-            RegistrationCommand = new RelayCommand(Registration);
+			RegistrationCommand = new RelayCommand(RegistrationUser);
         }
 
-        private void Registration(object obj)
+        private void RegistrationUser(object obj)
         {
-
+	        try
+	        {
+		        Registration.Register(Login, Password);
+	        }
+	        catch (Exception e)
+	        {
+		        Console.WriteLine(e);
+		        throw;
+	        }
         }
     }
 }
